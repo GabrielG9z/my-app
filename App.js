c17eefe032387c61d130c180584b27c1bbd411e8
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2023-01-17 11:46:35 -0300

    mapa

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform, StyleSheet } from "react-native";
-import * as ImagePicker from "expo-image-picker";
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
-export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
+export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -34,12 +13,9 @@ export default function ImagePickerExample() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  image: {
-    height: 150,
-    width: 150,
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
